5caeac69b03d7d8e2c626574791a8d6b6e297840
Mark Undezberx <yurasananas@gmail.com>
2026-06-20 17:40:57 +0300

    Remove git commands from main.py

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-echo "# Dragpolit-economy-bot" >> README.md
-git init
-git add README.md
-git commit -m "1 Version"
-git branch -M main
-git remote add origin https://github.com/yuras121/Dragpolit-economy-bot.git
-git push -u origin main
+
+111
